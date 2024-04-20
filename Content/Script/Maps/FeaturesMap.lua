a5d09f243e949dfccd1f62888a33f2e03de5431d
uezcr <75730965+uezcr@users.noreply.github.com>
2024-04-20 16:02:24 +0800

    创建项目

---
+++ Content/Script/Maps/FeaturesMap.lua
@@ -0,0 +1,42 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+local Screen = require "Library.Screen"
+
+---@type FeaturesMap_C
+local M = UnLua.Class()
+
+-- function M:Initialize(Initializer)
+-- end
+
+-- function M:UserConstructionScript()
+-- end
+
+function M:ReceiveBeginPlay()
+    local msg = [[
+    Lua!
+    ]]
+    print(msg)
+    Screen.Print(msg)
+end
+
+-- function M:ReceiveEndPlay()
+-- end
+
+-- function M:ReceiveTick(DeltaSeconds)
+-- end
+
+-- function M:ReceiveAnyDamage(Damage, DamageType, InstigatedBy, DamageCauser)
+-- end
+
+-- function M:ReceiveActorBeginOverlap(OtherActor)
+-- end
+
+-- function M:ReceiveActorEndOverlap(OtherActor)
+-- end
+
+return M

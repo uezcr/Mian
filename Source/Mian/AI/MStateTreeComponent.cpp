b8b4c1bfcec5fe800f0f750be8a53ff0eb70d892
uezcr <75730965+uezcr@users.noreply.github.com>
2024-04-28 02:05:44 +0800

    240428-AI测试

--- Source/Mian/AI/MStateTreeComponent.cpp
+++ Source/Mian/AI/MStateTreeComponent.cpp
@@ -1,3 +1,24 @@
 #include "AI/MStateTreeComponent.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(MStateTreeComponent)
+
+UMStateTreeComponent::UMStateTreeComponent()
+{
+}
+
+void UMStateTreeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UMStateTreeComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UMStateTreeComponent::SetStateTree(UStateTree* InStateTree)
+{
+	StateTreeRef.SetStateTree(InStateTree);
+	InitializeComponent();
+}
--- Source/Mian/AI/MStateTreeComponent.h
+++ Source/Mian/AI/MStateTreeComponent.h
@@ -7,5 +7,17 @@ UCLASS(ClassGroup = AI, HideCategories = (Activation, Collision), meta = (Bluepr
 class MIAN_API UMStateTreeComponent : public UStateTreeComponent
 {
 	GENERATED_BODY()
+
+public:
+	UMStateTreeComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable,Category="MStateTreeComponent")
+	void SetStateTree(UStateTree* InStateTree);
 	
 };
